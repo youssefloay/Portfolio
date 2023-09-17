1868220f2970183954bb51a8c17c6f5000d620b5
Youssef Loay <youssefloay@MacBook-Pro-de-Youssef.local>
2023-09-18 00:29:07 +0200

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import Header from "@/components/header";
 import "./globals.css";
 import { Inter } from "next/font/google";
@@ -32,6 +33,7 @@ export default function RootLayout({
           <ActiveSectionContextProvider>
             <Header />
             {children}
+            <Analytics />
             <Footer />
 
             <Toaster position="top-right" />
