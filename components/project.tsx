ac00ab8b49869f2009dc25a1322ab72717e78ca3
Youssef Loay <youssefloay@MacBook-Air-de-Youssef.local>
2025-01-01 14:23:54 +0100

    v2-2025

--- components/project.tsx
+++ components/project.tsx
@@ -2,10 +2,18 @@
 
 import { useRef } from "react";
 import { projectsData } from "@/lib/data";
-import Image from "next/image";
+//import Image from "next/image";
 import { motion, useScroll, useTransform } from "framer-motion";
+import Image, { StaticImageData } from "next/image";
 
-type ProjectProps = (typeof projectsData)[number];
+type ProjectProps = {
+  title: string;
+  description: string;
+  tags: readonly string[];
+  imageUrl: string | StaticImageData; // Accept both string and StaticImageData
+  githubUrl?: string;
+  websiteUrl?: string;
+};
 
 export default function Project({
   title,
@@ -49,58 +57,34 @@ export default function Project({
             ))}
           </ul>
           <div className="mt-5 flex gap-3">
-  {githubUrl && (
-    <a
-      href={githubUrl}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="bg-black text-white px-4 py-2 rounded-full flex items-center gap-2"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="currentColor"
-        className="w-6 h-6"
-      >
-        <path
-          fillRule="evenodd"
-          d="M12 2C6.477 2 2 6.477 2 12c0 4.418 2.865 8.166 6.839 9.489.5.092.682-.217.682-.482 0-.237-.009-.868-.013-1.703-2.782.605-3.369-1.34-3.369-1.34-.454-1.154-1.11-1.462-1.11-1.462-.908-.62.069-.607.069-.607 1.004.07 1.532 1.032 1.532 1.032.892 1.528 2.341 1.087 2.91.832.092-.647.35-1.087.636-1.337-2.22-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.984 1.029-2.683-.103-.253-.446-1.27.098-2.646 0 0 .84-.269 2.75 1.025A9.564 9.564 0 0112 6.8c.85.004 1.705.115 2.504.337 1.91-1.294 2.75-1.025 2.75-1.025.544 1.376.201 2.393.099 2.646.64.699 1.028 1.592 1.028 2.683 0 3.842-2.337 4.687-4.563 4.936.359.31.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .267.18.578.688.48C19.135 20.165 22 16.418 22 12c0-5.523-4.477-10-10-10z"
-          clipRule="evenodd"
-        />
-      </svg>
-      GitHub
-    </a>
-  )}
+            {githubUrl && (
+              <a
+                href={githubUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bg-black text-white px-4 py-2 rounded-full flex items-center gap-2"
+              >
+                GitHub
+              </a>
+            )}
             {websiteUrl && (
-    <a
-      href={websiteUrl}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="bg-blue-500 text-white px-4 py-2 rounded-full flex items-center gap-2"
-    >
-      
-      Website
-    </a>
-  )}
-</div>
+              <a
+                href={websiteUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bg-blue-500 text-white px-4 py-2 rounded-full flex items-center gap-2"
+              >
+                Website
+              </a>
+            )}
+          </div>
         </div>
 
         <Image
           src={imageUrl}
           alt="Project I worked on"
           quality={95}
-          className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
-        transition 
-        group-hover:scale-[1.04]
-        group-hover:-translate-x-3
-        group-hover:translate-y-3
-        group-hover:-rotate-2
-
-        group-even:group-hover:translate-x-3
-        group-even:group-hover:translate-y-3
-        group-even:group-hover:rotate-2
-
-        group-even:right-[initial] group-even:-left-40"
+          className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition group-hover:scale-[1.04] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2 group-even:right-[initial] group-even:-left-40"
         />
       </section>
     </motion.div>
--- lib/data.ts
+++ lib/data.ts
@@ -6,6 +6,7 @@ import albinome from "@/public/albinome.png";
 import rmtdevImg from "@/public/rmtdev.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
 import wtw from "@/public/wtw.png";
+
 export const links = [
   {
     name: "Home",
@@ -38,8 +39,7 @@ export const experiencesData = [
     title: "Web Developer",
     location: "Montpellier, France",
     description:
-      "  As a Web Developer Intern based in the picturesque city of Montpellier, France, I embarked on a dynamic and immersive two-month journey. During this time, I had the invaluable opportunity to work on real-world projects, collaborate with experienced professionals, and further refine my web development skills. My internship experience not only expanded my technical knowledge but also provided me with practical insights into the fast-paced world of web development.",
-      
+      "As a Web Developer Intern based in the picturesque city of Montpellier, France, I embarked on a dynamic and immersive two-month journey. During this time, I had the invaluable opportunity to work on real-world projects, collaborate with experienced professionals, and further refine my web development skills. My internship experience not only expanded my technical knowledge but also provided me with practical insights into the fast-paced world of web development.",
     icon: React.createElement(CgWorkAlt),
     date: "2020",
   },
@@ -49,16 +49,13 @@ export const experiencesData = [
     description:
       "During my time at École Supérieure de Génie Informatique (ESGI) in Lyon, France, I embarked on an enriching educational journey that spanned three transformative years. This period of academic exploration and personal growth culminated in the successful completion of my Bachelor's degree.",
     icon: React.createElement(LuGraduationCap),
-   
     date: "2019 - 2022",
   },
-  
   {
-    
-      title: "Freelance Developer",
-      location: "Montpellier, France",
-      description:
-        "As a Freelance Developer, I've been dedicated to providing innovative solutions and exceptional services. My focus lies in crafting responsive and mobile-adaptive web applications. I specialize in optimizing performance and have a keen eye for detail when it comes to design and prototyping, using tools like Figma to bring concepts to life. I thrive in delivering high-quality, tailored solutions to my clients, ensuring their digital presence is both seamless and efficient.",
+    title: "Freelance Developer",
+    location: "Montpellier, France",
+    description:
+      "As a Freelance Developer, I've been dedicated to providing innovative solutions and exceptional services. My focus lies in crafting responsive and mobile-adaptive web applications. I specialize in optimizing performance and have a keen eye for detail when it comes to design and prototyping, using tools like Figma to bring concepts to life. I thrive in delivering high-quality, tailored solutions to my clients, ensuring their digital presence is both seamless and efficient.",
     icon: React.createElement(FaReact),
     date: "2022 - present",
   },
@@ -67,22 +64,22 @@ export const experiencesData = [
 export const projectsData = [
   {
     title: "Albinome",
-    description: "I worked as a full-stack developer on this Albinome food truck project. It's a platform for customers to easily order delicious pizza.",
-
-    tags: ["React", "Figma", "Tailwind","Firebase",],
+    description:
+      "I worked as a full-stack developer on this Albinome food truck project. It's a platform for customers to easily order delicious pizza.",
+    tags: ["React", "Figma", "Tailwind", "Firebase"],
     imageUrl: albinome,
+    githubUrl: "", // Optional property
+    websiteUrl: "", // Optional property
   },
-
   {
     title: "Where to watch",
-    description: "Web app for searching movies and trailers, showing where to stream them. Aggregates data from various sources for easy film discovery.",
-
-    tags: ["React", "TypeScript", "Tailwind","Netlify",],
+    description:
+      "Web app for searching movies and trailers, showing where to stream them. Aggregates data from various sources for easy film discovery.",
+    tags: ["React", "TypeScript", "Tailwind", "Netlify"],
     imageUrl: wtw,
     githubUrl: "https://github.com/youssefloay/WhereToWatch",
-    websiteUrl: "https://iridescent-toffee-0dc7bd.netlify.app.",
+    websiteUrl: "https://iridescent-toffee-0dc7bd.netlify.app",
   },
-  
 ] as const;
 
 export const skillsData = [
